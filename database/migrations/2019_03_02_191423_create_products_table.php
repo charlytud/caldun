a87a201ccfd341eabe785a07fd11b837edfca74c
charlytud <charlytud@gmail.com>
2019-03-03 04:21:54 +0200

    migrations products and reviews

--- database/migrations/2019_03_02_191423_create_products_table.php
+++ database/migrations/2019_03_02_191423_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->double('price, 8, 2');
+            $table->integer('stock');
+            $table->double('discount, 8, 2');
             $table->timestamps();
         });
     }
